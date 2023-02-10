fa0f2c64746f6d246c05abc6c8b5996ea4e40a5d
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-02-10 18:34:25 +0530

    update api's name conventions

--- DistributionChannel/DistributionChannel/urls.py
+++ DistributionChannel/DistributionChannel/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/v1/',include('micro_auth_service.urls')),
-    path('manage/brand/v1/',include('micro_brandCategoryProduct_service.urls.brand_urls')),
-    path('manage/category/v1/',include('micro_brandCategoryProduct_service.urls.category_urls')),
-    path('manage/product/v1/',include('micro_brandCategoryProduct_service.urls.product_urls'))
+    path('api/v1/auth/',include('micro_auth_service.urls')),
+    path('api/v1/manage/brand/',include('micro_brandCategoryProduct_service.urls.brand_urls')),
+    path('api/v1/manage/category/',include('micro_brandCategoryProduct_service.urls.category_urls')),
+    path('api/v1/manage/product/',include('micro_brandCategoryProduct_service.urls.product_urls'))
 ]
