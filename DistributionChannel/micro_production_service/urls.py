528a3441836772ad7d43d21e0f869393d55d93d2
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-02-12 13:04:52 +0530

    add apis

--- DistributionChannel/micro_production_service/urls.py
+++ DistributionChannel/micro_production_service/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('update_production/user_id=<str:userid>&product_id=<str:productid>',update_production),
-    path('get_total_production_of_a_product/user_id=<str:userid>&product_id=<str:productid>',get_total_production_of_a_product)
+    path('get_total_production_of_a_product/user_id=<str:userid>&product_id=<str:productid>',get_total_production_of_a_product),
+    path('get_total_production_of_a_user/user_id=<str:userid>',get_total_production_of_a_user)
 ]
--- DistributionChannel/micro_production_service/views.py
+++ DistributionChannel/micro_production_service/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 import uuid
 from micro_auth_service.models import BusinessUsers
 from Authorization.authorization import Verify
-from micro_brandCategoryProduct_service.models import Product
+from micro_brandCategoryProduct_service.models import Product,Brand,ProductCategory
 from .models import Production
 import json
 from VerifyAccess.access import verifyAccess
@@ -55,6 +55,7 @@ def get_a_single_production(product):
             obj=Production.objects.filter(product=product)
             for p in range(0,obj.count()):
                 data={
+                    "product_name":(Product.objects.get(product_id=obj.values('product')[p]['product'])).name,
                     "production_id":obj.values('production_id')[p]['production_id'],
                     "production_rate":obj.values('production_rate')[p]['production_rate'],
                     "production_date":obj.values('production_date')[p]['production_date']
@@ -80,6 +81,55 @@ def get_total_production_of_a_product(request,userid,productid):
             product=Product.objects.get(product_id=productid)
             if(verifyAccess(request,ACCESSED_BY) and Verify(request).is_Tokenvalid()['userid']==userid and product.category.brand.user==user):
                 data=get_a_single_production(product)
+                return Response({"data":data},status=status.HTTP_200_OK)
+        except:
+            pass
+        else:
+            return Response({"info":"access denied!"},status=status.HTTP_406_NOT_ACCEPTABLE)
+             
+    return Response({"info":"Something wrong!"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+def get_a_users_production(user):
+    try:
+        production=[]    
+        product_list=set()
+
+        if(Production.objects.exists()):
+            obj=Production.objects.all()
+            for i in range(0,obj.count()):  
+                if(Product.objects.get(product_id=obj.values('product')[i]['product']).category.brand.user==user):
+                    product_list.add(Product.objects.get(product_id=obj.values('product')[i]['product']))
+            
+            for pro in list(product_list):
+                obj_p=Production.objects.filter(product=pro)
+                for p in range(0,obj_p.count()):    
+                    data={
+                        "product_name":(Product.objects.get(product_id=obj_p.values('product')[p]['product'])).name,
+                        "production_id":obj_p.values('production_id')[p]['production_id'],
+                        "production_rate":obj_p.values('production_rate')[p]['production_rate'],
+                        "production_date":obj_p.values('production_date')[p]['production_date']
+                    }
+                    production.append(data)              
+
+        return production
+    except:
+        pass
+
+@api_view(['GET'])
+def get_total_production_of_a_user(request,userid):
+    if(Verify(request).is_Tokenvalid()['status']==401):
+        return Response({"info":"UnAuthorized"},status=status.HTTP_401_UNAUTHORIZED)
+    
+    elif (Verify(request).is_Tokenvalid()['status']==403):
+        return Response({"info":"invalid token"},status=status.HTTP_403_FORBIDDEN)
+    
+    elif(Verify(request).is_Tokenvalid()['status']==200):
+        try:
+            user=BusinessUsers.objects.get(uid=userid)
+            if(verifyAccess(request,ACCESSED_BY) and Verify(request).is_Tokenvalid()['userid']==userid):
+                data=get_a_users_production(user)
                 return Response({"data":data},status=status.HTTP_200_OK)
         except:
             pass
